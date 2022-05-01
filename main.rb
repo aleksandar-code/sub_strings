3755493f4aefc9eaebbbf74aa07daddcdcea6af3
Shisui <aca.jevdjevic@gmail.com>
2022-05-01 20:18:08 +0200

    Cleaning indentation

--- main.rb
+++ main.rb
@@ -1,40 +1,27 @@
-require 'pry-byebug'
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def sub_strings(words_array, dictionary)
-    found_var_array = []
-    stack_letters = ""
-    
-    
 
+    found_var_array = []
     words_array.each do |word|
-        
+
         word = word.downcase
-        
-                dictionary.each do |word_inside|
-                    if dictionary.include?(word) || word.include?(word_inside)
-                        
-                        if word.include?(word_inside) 
-                           found_var_array << word_inside
-                            
-                        end
-                        
-                    end
-                    
+        dictionary.each do |word_inside|
+
+            if dictionary.include?(word) || word.include?(word_inside)
+                if word.include?(word_inside) 
+                   found_var_array << word_inside
                 end
-            
+            end
+        end    
     end
-    
-    
+
     the_hash = found_var_array.reduce(Hash.new(0)) do |result, occurrence|
         result[occurrence] += 1
-        result
-        
+        result 
     end
+    
     print "the hash#{the_hash}\n\n"
-
-    
-    
 end
 
 print "Enter some text: "
