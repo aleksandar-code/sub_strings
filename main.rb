73943abb160c1a0c9ca5bb6675c5ff25fb291c42
Shisui <aca.jevdjevic@gmail.com>
2022-05-01 15:32:07 +0200

    the program works with a few bugs

--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
+require 'pry-byebug'
 # Pseudocode is what to do => Problem solving is how to do.
 
 # Create variables; word, dictionary. #
 # The word is split in an array of chars
 # Both are sent to the method as arguments
+binding.pry
 word = "not"
 dictionary = ["to", "be", "or", "not", "below"]
 word_array = word.split("")
@@ -10,26 +12,31 @@ word_array = word.split("")
 # Iterate through word_array with an each loop
 
 def sub_strings(word_array, dictionary)
+    binding.pry
     found_var_array = []
+    stack_letters = ""
     word_array.each do |letter|
-        stack_letters = []
-        stack_letters << letter
-
+        
+        
+        stack_letters += letter
+        binding.pry
+        
         if dictionary.include?(stack_letters)
-            found_var_array = stack_letters
+            found_var_array << stack_letters
         end
 
     end
-
+    binding.pry
+    
     the_hash = found_var_array.reduce(Hash.new(0)) do |result, occurrence|
         result[occurrence] +=1
         result
     end
-
+    binding.pry
     print the_hash
 end
-        
-
+binding.pry
+sub_strings(word_array, dictionary)
 # What happens in the each loop? #
 # Create variables; stack_letters, found_var_array
 
