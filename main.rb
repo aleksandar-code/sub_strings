562cf040acaeec76dd7c5999320564b3f412d927
Shisui <aca.jevdjevic@gmail.com>
2022-05-01 19:48:05 +0200

    Works alike odin's example

--- main.rb
+++ main.rb
@@ -1,22 +1,10 @@
 require 'pry-byebug'
-# Pseudocode is what to do => Problem solving is how to do.
-
-# Create variables; word, dictionary. #
-# The word is split in an array of chars
-# Both are sent to the method as arguments
-binding.pry
-
-# What happpens in the method? #
-# Iterate through word_array with an each loop
-
-dictionary = ["I", "of", "and", "a", "to", "in", "is", "you", "that", "it", "he", "was", "for", "on", "are", "as", "with", "his", "they"]
-
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def sub_strings(words_array, dictionary)
-    binding.pry
     found_var_array = []
     stack_letters = ""
-    the_hash = {}
+    
     
 
     words_array.each do |word|
@@ -24,50 +12,41 @@ def sub_strings(words_array, dictionary)
         word = word.split("")
 
         stack_letters = ""
-
-            word.each do |letter|
-            
-                stack_letters += letter
-                binding.pry
-                
-                if dictionary.include?(stack_letters)
-                    found_var_array << stack_letters
-                end
-                
-            end
         
-      
-    end
+            word.each do |letter|
+                letter = letter.downcase
+                stack_letters += letter
+                
+                
 
-  
+                dictionary.each do |word_inside|
+                    if dictionary.include?(stack_letters)
+                        
+                        if stack_letters.include?(word_inside) 
+                            found_var_array << word_inside
+                            
+                        end
+                        
+                    end
+                    
+                end
+            end
+    end
     
-    print "#{found_var_array}\n\n"
+    
+    the_hash = found_var_array.reduce(Hash.new(0)) do |result, occurrence|
+        result[occurrence] += 1
+        result
+        
+    end
+    print "the hash#{the_hash}\n\n"
+
+    
     
 end
 
-
+print "Enter some text: "
 words = gets.chomp
 words_array = words.split(" ")
 
 sub_strings(words_array, dictionary)
-
-
-
-# What happens in the each loop? #
-# Create variables; stack_letters, found_var_array
-
-# Those will iterate over one word
-# dictionary.include?(stack_letters) => if true
-# give the stack_letters to found_var_array
-
-# Then found_var_array.reduce(Hash.new(0))
-# Print the result
-
-
-
- 
-# the_hash = found_var_array.reduce do |result, occurrence|
-#     result[occurrence] +=1
-#     result
-    
-# end
