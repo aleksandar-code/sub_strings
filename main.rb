0c5018b338cb3ec220ab6a38fc4b763c1602edf8
Shisui <aca.jevdjevic@gmail.com>
2022-05-01 14:48:12 +0200

    Plan laid out

---
+++ main.rb
@@ -0,0 +1,19 @@
+# Pseudocode is what to do => Problem solving is how to do.
+
+# Create variables; word, dictionary. #
+# The word is split in an array of chars
+# Both are sent to the method as arguments
+
+# What happpens in the method? #
+# Iterate through word_array with an each loop
+
+# What happens in the each loop? #
+# Create variables; stack_letters, found_var_array
+
+# Those will iterate over one word
+# dictionary.include?(stack_letters) => if true
+# give the stack_letters to found_var_array
+
+# Then found_var_array.reduce(Hash.new(0))
+# Print the result
+
